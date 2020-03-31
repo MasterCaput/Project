36f59e2677f7e077dc2d2b64d053e477b1849921
MasterCaput <wasilisitnikov000@yandex.ru>
2020-03-31 22:13:41 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,8 +1,21 @@
-"use strict";
-alert("Снова здарова");
-let num;
-num = (2 + 5 * 7896);
-alert("Дрочить разрешаю");
-alert(num);
-alert("СПЕМЫЧ");
-open("jpg.jpg");
+let start = Date.now(); // запомнить время начала
+
+let timer = setInterval(function() {
+    // сколько времени прошло с начала анимации?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 3000) {
+        clearInterval(timer); // закончить анимацию через 3 секунды
+        return;
+    }
+
+    // отрисовать анимацию на момент timePassed, прошедший с начала анимации
+    draw(timePassed);
+
+}, 30);
+
+// в то время как timePassed идёт от 0 до 2000
+// left изменяет значение от 0px до 400px
+function draw(timePassed) {
+    train.style.left = timePassed / 3 + 'px';
+}
